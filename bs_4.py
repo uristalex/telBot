93a9c0ef5e6e92bbc41fe8db526ad9d1b1787c7c
Alex <uristalex@gmail.com>
2024-05-12 22:40:30 +0300

    Newdef

---
+++ bs_4.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import random
+
+spisok_f = []
+
+
+def spisok_500():
+    global spisok_f
+    if len(spisok_f) == 0:
+        for i in (1, 2):
+            response_get = requests.get(f'https://www.kinoafisha.info/rating/movies/?page={i}')
+            print(response_get.status_code)
+            soup = bs(response_get.text, features='html.parser')
+            list_films = soup.find_all('a', class_='movieItem_title')
+            for film in list_films:
+
+                link_pic = soup.find(alt=f'{film.text}')
+                spisok_f.append([film.text, str(link_pic)])
+
+        return random.choice(spisok_f)
+    else:
+        return random.choice(spisok_f)
+
+
+spisok_500()
+print(spisok_f)
+
+
+# spisok_500()
+# print(spisok_f)
+
+
+
+
+#<img class="picture_image" alt="Шрэк" title="Шрэк" src="https://static.kinoafisha.info/k/movie_posters/220/upload/movie_posters/5/2/6/5625/8971a949ea7f23e16f77f34c1d9403aa.jpeg">
