236b28554e09b0595dda626e7fbc56a94c58fe90
uristalex <uristalex@gmail.com>
2024-05-28 11:02:24 +0300

    пробы пера :)

---
+++ AioTest/AioBotTest.py
@@ -0,0 +1,29 @@
+import requests
+import time
+from config import TOKEN
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = TOKEN
+TEXT = 'Ура! Классный апдейт!'
+MAX_COUNTER = 100
+
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
