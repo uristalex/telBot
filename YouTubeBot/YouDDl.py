c3c6ca7dc29d0d445df603efabf5044f491256bb
Alex <uristalex@gmail.com>
2024-06-04 22:54:06 +0300

    Update sam func

--- YouTubeBot/YouDDl.py
+++ YouTubeBot/YouDDl.py
@@ -1,7 +1,9 @@
 from yt_dlp import YoutubeDL
 import os
+from pathlib import Path
+import shutil
 
-limit_vid_size: float = 10
+limit_vid_size: float = 100
 total_vid_size: float = 0
 
 def split_name(title_n: str) -> str:
@@ -17,11 +19,16 @@ def split_name(title_n: str) -> str:
     return text_name
 
 
-# def sum_file_vid(folder: str):
-#     size_folder: float = 0
-#     for i in os.listdir(folder):
-#         size_folder+=os.path.getsize(i)
-#     return size_folder
+def folder_Size(path: str)-> float:
+    """Функция подсчета размера папки для временного хранения файлов\n
+    необходима для контроля объема загружаемых данных\n
+    Параметры:\n
+    path: str - принимает имя папки(путь) где хранятся файлы для подсчета"""
+    return round((sum([os.path.getsize(i) for i in Path(path).rglob('*')])/1024)/1024, 2)
+
+def delete_everything_in_folder(folder_path):
+    shutil.rmtree(folder_path)
+    os.mkdir(folder_path)
 
 
 
@@ -40,12 +47,16 @@ def download_vid(url: str, sefe_server: bool = False) -> str:
     Строка с именем файла для дальнейшей обработки в боте.
     """
     global total_vid_size
-    # print(sum_file_vid('videos'))
+    flag: str = ''
     ydl_opts = {
         'quiet': True,
         'format': 'mp4',
         'no_warnings': True
     }
+    if folder_Size('videos') > limit_vid_size:
+        delete_everything_in_folder('videos')
+        print('deleted')
+
     try:
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
@@ -53,24 +64,24 @@ def download_vid(url: str, sefe_server: bool = False) -> str:
             ydl_opts['outtmpl']['default'] = f'server/videos/{split_name(info["title"])}.mp4'
             ydl.download([url])
             print(f"{ydl_opts['outtmpl']['default']} safe_server")
-            return f'server'
+            flag = f'server'
 
         size_info = round((info['filesize_approx'] / 1024) / 1024, 2)
-        if size_info:
-            print(size_info)
         if size_info > 49:
             print(f'Fail to Big: {size_info}')
-            return f'big'
+            flag = f'big'
         else:
             ydl_opts['outtmpl']['default'] = f'videos/{split_name(info["title"])}.mp4'
             ydl.download([url])
             total_vid_size += size_info
             print(f"{ydl_opts['outtmpl']['default']} size {size_info}")
             print(total_vid_size)
-
-            return ydl_opts['outtmpl']['default']
+            flag = ydl_opts['outtmpl']['default']
     except Exception as err:
         print(err)
-        return 'Er'
+        flag = 'Er'
+
+
+    return flag
 
 # download_vid('https://youtube.com/shorts/VNpFR1ow-3U?si=429A7X6-oCCI5-Wb')
