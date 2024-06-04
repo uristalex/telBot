52e64b53ed35b5e8d91767083433d6ae7e7e8dbd
uristalex <uristalex@gmail.com>
2024-06-04 16:27:24 +0300

    пробы пера :)

--- YouTubeBot/YouDDl.py
+++ YouTubeBot/YouDDl.py
@@ -1,5 +1,8 @@
 from yt_dlp import YoutubeDL
+import os
 
+limit_vid_size: float = 10
+total_vid_size: float = 0
 
 def split_name(title_n: str) -> str:
     """Функция удаления специальных символов
@@ -12,6 +15,14 @@ def split_name(title_n: str) -> str:
             continue
         text_name += i
     return text_name
+
+
+# def sum_file_vid(folder: str):
+#     size_folder: float = 0
+#     for i in os.listdir(folder):
+#         size_folder+=os.path.getsize(i)
+#     return size_folder
+
 
 
 def download_vid(url: str, sefe_server: bool = False) -> str:
@@ -28,6 +39,8 @@ def download_vid(url: str, sefe_server: bool = False) -> str:
 
     Строка с именем файла для дальнейшей обработки в боте.
     """
+    global total_vid_size
+    # print(sum_file_vid('videos'))
     ydl_opts = {
         'quiet': True,
         'format': 'mp4',
@@ -36,18 +49,25 @@ def download_vid(url: str, sefe_server: bool = False) -> str:
     try:
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
-        ydl_opts['outtmpl']['default'] = f'videos/{split_name(info["title"])}.mp4'
         if sefe_server:
+            ydl_opts['outtmpl']['default'] = f'server/videos/{split_name(info["title"])}.mp4'
             ydl.download([url])
             print(f"{ydl_opts['outtmpl']['default']} safe_server")
             return f'server'
+
         size_info = round((info['filesize_approx'] / 1024) / 1024, 2)
+        if size_info:
+            print(size_info)
         if size_info > 49:
             print(f'Fail to Big: {size_info}')
             return f'big'
         else:
+            ydl_opts['outtmpl']['default'] = f'videos/{split_name(info["title"])}.mp4'
             ydl.download([url])
+            total_vid_size += size_info
             print(f"{ydl_opts['outtmpl']['default']} size {size_info}")
+            print(total_vid_size)
+
             return ydl_opts['outtmpl']['default']
     except Exception as err:
         print(err)
