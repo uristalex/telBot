242ad3b5ffbdb874115821e460eb390a5d6203d3
Alex <uristalex@gmail.com>
2024-06-07 21:02:45 +0300

    Update sam func

--- YouTubeBot/test.py
+++ YouTubeBot/test.py
@@ -1,2 +1,21 @@
-stroka = 'shttps'
-print(stroka.lstrip('s'))
+# import os
+#
+# s = 0
+# for a, b, f in os.walk('videos/'):
+#     print(os.path.getsize(f))
+#
+# print(s)
+
+import os
+from pathlib import Path
+
+#Вычисляет размер папки, количество файлов и количество итераций функции
+def folderSize(path):
+    # fsize = 0
+    # for file in Path(path).rglob('*'):
+    #     if (os.path.isfile(file)):
+    #         fsize += os.path.getsize(file)
+    # return round((fsize/1024), 2)
+    return round(sum([os.path.getsize(i) for i in Path(path).rglob('*')])/1024, 2)
+
+print(folderSize('videos'))
